b5fa2d0f3d8cc67a5a1e7ce4d0adccacc266e9df
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2018-05-05 21:52:39 -0500

    Agregando DropDown

--- PaginaMaterialize/js/app.js
+++ PaginaMaterialize/js/app.js
@@ -3,6 +3,7 @@
   $(document).ready(function(){
     $('.parallax').parallax();
      $('.slider').slider();
+     $(".dropdown-trigger").dropdown();
   $('.carousel.carousel-slider').carousel({
     fullWidth: true
     
