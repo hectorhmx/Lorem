b8aac4016ffe04637e9ad253418019b4dfd98e09
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2018-05-08 02:06:34 -0500

    MEjorando

--- PaginaMaterialize/js/app.js
+++ PaginaMaterialize/js/app.js
@@ -1,18 +1,4 @@
-  
-    // Or with j  
-    $(document).ready(function(){
-      $('.sidenav').sidenav();
-    });
-
   $(document).ready(function(){
     $('.parallax').parallax();
-     $('.slider').slider();
-     $(".dropdown-trigger").dropdown();
-     $(".dropdown-trigger").dropdown();
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true
-    
-  }); 
+    $(".dropdown-trigger").dropdown();
   });
-
- 
