06a67d7c16e34fe6f998ba432bf22426bae0c126
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2018-05-08 23:28:49 -0500

    Bugfixed292

--- PaginaMaterialize/js/app.js
+++ PaginaMaterialize/js/app.js
@@ -1,9 +1,5 @@
   $(document).ready(function(){
-  	$('.target').pushpin({
-    top: 0,
-    bottom: 1000,
-    offset: 0
-  });
+
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
