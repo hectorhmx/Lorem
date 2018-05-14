168e1011c42bb4013fe58c7c2a07a587d5cd97f4
emanuelFI <mufymes@gmail.com>
2018-05-14 00:10:16 -0500

    BuGfIxEd

--- loremproject/loremproject/settings.py
+++ loremproject/loremproject/settings.py
@@ -137,4 +137,9 @@ if DEBUG:
     EMAIL_FILE_PATH  = os.path.join(BASE_DIR, "sent_emails")
 else:
     # Aquí hay que configurar un email real para producción
-    pass
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = 'eabetanlu@gmail.com'
+    EMAIL_HOST_PASSWORD = '1234asdfqwer'
+    EMAIL_PORT = 587
+
